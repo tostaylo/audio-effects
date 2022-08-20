d89eed760e4353e28ca1697d7334b708afbc389a
Torre <tostaylo@gmail.com>
2022-08-20 13:16:38 -0500

    Add husky pre-commit hook for prettier

--- public/js/distortion.js
+++ public/js/distortion.js
@@ -8,6 +8,11 @@ export default function Distortion(audioContext) {
 export function createDistortionCurve(amount = 20) {
   let n_samples = 256,
     curve = new Float32Array(n_samples);
+
+
+
+
+
   for (let i = 0; i < n_samples; ++i) {
     let x = (i * 2) / n_samples - 1;
     curve[i] = ((Math.PI + amount) * x) / (Math.PI + amount * Math.abs(x));
