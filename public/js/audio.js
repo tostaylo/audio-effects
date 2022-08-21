48ce86522c44f4af38c30220433cc7de8e40a988
Torre <tostaylo@gmail.com>
2022-08-21 17:35:46 -0500

    #2 added first impulse response. Modified Gain function to use a constructor for creating Gain Nodes.

--- public/js/audio.js
+++ public/js/audio.js
@@ -3,27 +3,33 @@ import Distortion from './distortion.js';
 import Gain from './gain.js';
 import createTrack from './track.js';
 import waveformVisualizer from './visualizer.js';
+import impulseResponse from './impulse-response.js';
 
-export default function initialize() {
+export default async function initialize() {
   const audioContext = new AudioContext();
   const audioElement = document.querySelector('audio');
 
   const track = createTrack(audioContext, audioElement);
 
+  const convolver = await impulseResponse(audioContext);
   const analyser = audioContext.createAnalyser();
 
   waveformVisualizer(analyser);
 
   const distortionNode = Distortion(audioContext);
-  const gainNode = Gain(audioContext);
+
+  const preAmpGainNode = Gain(audioContext);
+  const postAmpGainNode = Gain(audioContext, { gain: 2 });
 
   track
-    .connect(gainNode)
+    .connect(preAmpGainNode)
     .connect(distortionNode)
+    .connect(convolver)
+    .connect(postAmpGainNode)
     .connect(analyser)
     .connect(audioContext.destination);
 
-  DOM({ gainNode, distortionNode }, audioContext);
+  DOM({ gainNode: preAmpGainNode, distortionNode }, audioContext);
 }
 
-initialize();
+await initialize();
---
+++ public/js/impulse-response.js
@@ -0,0 +1,13 @@
+export default async function impulseResponse(context) {
+  const convolver = new ConvolverNode(context);
+
+  const response = await fetch(
+    'rocksta-reactions-fender-twin-reverb-seiren-pro-l-a-8-4-45-45.wav'
+  );
+  const buffer = await response.arrayBuffer();
+  await context.decodeAudioData(buffer, (decoded) => {
+    convolver.buffer = decoded;
+  });
+
+  return convolver;
+}
