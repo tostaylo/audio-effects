7f3066cf1b48bb2764487ecdaee18298a74451fc
Charles <cchester1@angelo.edu>
2022-08-22 10:26:31 -0500

    #1 editing the Reverb function. Overloading the track somehow.

--- public/js/reverb.js
+++ public/js/reverb.js
@@ -2,16 +2,15 @@
 // fetch a wav doc and apply the arrayBuffer method to it
 // use the decodeAudioData on the manipulated wav and apply the result as the value of the buffer property of createConvolver
 // return end product to acheive reverb effect
-export default function Reverb(audioContext) {
+export default async function Reverb(audioContext) {
   let reverb = audioContext.createConvolver();
 
-  fetch('../vocal_duo.wav')
-    .then((response) => response.arrayBuffer())
-    .then((data) => {
-      audioContext.decodeAudioData(data, (result) => {
-        reverb.buffer = result;
-      });
-    });
+  const response = await fetch('vocal_duo.wav');
+  const buffer = await response.arrayBuffer();
+
+  await audioContext.decodeAudioData(buffer, (result) => {
+    reverb.buffer = result;
+  });
 
   return reverb;
 }
