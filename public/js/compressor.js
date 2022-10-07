7d0340d4044c02b7565e6fb52670af43e17f27cf
Charles <cchester1@angelo.edu>
2022-10-06 22:12:02 -0500

    #31 compressor module edited

--- public/js/compressor.js
+++ public/js/compressor.js
@@ -5,4 +5,6 @@ export default function Compressor(audioContext) {
   compressor.ratio.setValueAtTime(15, audioContext.currentTime);
   compressor.attack.setValueAtTime(5, audioContext.currentTime);
   compressor.release.setValueAtTime(-1, audioContext.currentTime);
+
+  return compressor;
 }
