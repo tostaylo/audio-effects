3f51ad55af7fb25aa1898c39f5058cf0e4a7f18d
Charles <cchester1@angelo.edu>
2022-10-06 22:09:39 -0500

    compressor module written

---
+++ public/js/compressor.js
@@ -0,0 +1,8 @@
+export default function Compressor(audioContext) {
+  const compressor = audioContext.createDynamicsCompressor();
+  compressor.threshold.setValueAtTime(-40, audioContext.currentTime);
+  compressor.knee.setValueAtTime(40, audioContext.currentTime);
+  compressor.ratio.setValueAtTime(15, audioContext.currentTime);
+  compressor.attack.setValueAtTime(5, audioContext.currentTime);
+  compressor.release.setValueAtTime(-1, audioContext.currentTime);
+}
