52fa6b6279f30bff8488e62130e3b4cea095444a
Charles Chester <cchester1@angelo.edu>
2022-10-23 20:07:38 -0500

    31 create compression effect (#33)
    # What Changed
    
    compressorNode module written
    compressorNode passed through audio.js
    
    ## Design
    
    module architected similarly to previously written modules.
    
    ## Areas Affected
    
    Audio.js
    
    ## How to Test
    
    Test by commenting out the compressorNode in the nodes array of audio.js

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,5 +1,6 @@
 import DOM from './dom.js';
 import Reverb from './reverb.js';
+import Compressor from './compressor.js';
 import Delay from './delay.js';
 import Distortion from './distortion.js';
 import Gain from './gain.js';
@@ -27,6 +28,13 @@ export default async function initialize() {
   const preAmpGainNode = Gain(audioContext);
   const postAmpGainNode = Gain(audioContext, { gain: 2 });
   const reverbNode = await Reverb(audioContext);
+  const compressorNode = await Compressor(audioContext, {
+    threshold: -40,
+    knee: 30,
+    attack: 1,
+    release: 1,
+    ratio: 15,
+  });
 
   const nodes = [
     preAmpGainNode,
@@ -34,6 +42,7 @@ export default async function initialize() {
     distortionNode,
     convolver,
     reverbNode,
+    compressorNode,
     postAmpGainNode,
     analyser,
     audioContext.destination,
---
+++ public/js/compressor.js
@@ -0,0 +1,11 @@
+export default function Compressor(audioContext, options = {}) {
+  const { currentTime } = audioContext;
+  const compressor = audioContext.createDynamicsCompressor();
+  compressor.threshold.setValueAtTime(options.threshold, currentTime);
+  compressor.knee.setValueAtTime(options.knee, currentTime);
+  compressor.attack.setValueAtTime(options.attack, currentTime),
+    compressor.release.setValueAtTime(options.release, currentTime);
+  compressor.ratio.setValueAtTime(options.ratio, currentTime);
+
+  return compressor;
+}
