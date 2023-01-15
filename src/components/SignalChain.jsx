5a9398b2f1a8eb9cf60bf0dd664c53a31ccb4a12
Torre <tostaylo@gmail.com>
2023-01-14 22:05:28 -0600

    styling for signal blocks to have space around them and take up full screen

--- src/components/SignalChain.jsx
+++ src/components/SignalChain.jsx
@@ -3,7 +3,7 @@ import { SignalBlock } from './SignalBlock';
 
 export function SignalChain() {
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
       {[1, 2, 3, 4, 5].map((item) => (
         <SignalBlock key={item} />
       ))}
