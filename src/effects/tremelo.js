3299b264d6bde4e533c9f44cc98fb8173af8c38f
Charles <cchester1@angelo.edu>
2023-01-18 10:10:35 -0600

    No message

--- src/effects/tremelo.js
+++ src/effects/tremelo.js
@@ -4,6 +4,21 @@ const defaultOptions = {
   frequency: 440,
 };
 
-export default function Tremelo(audioContext, options = defaultOptions) {
-  console.log(options, audioContext);
+function Oscillator(audioContext, options = defaultOptions) {
+  const { currentTime } = audioContext;
+  const tremelo = audioContext.createOscillator();
+
+  tremelo.type.setValueAtTime(options.type, currentTime);
+  tremelo.detune.setValueAtTime(options.detune, currentTime);
+  tremelo.frequency.setValueAtTime(options.frequency, currentTime);
+
+  return [tremelo];
+}
+
+export default function Tremelo(audioContext) {
+  const gainNode = new GainNode(audioContext, 2);
+
+  const tremelo = Oscillator.start();
+
+  return [gainNode, tremelo];
 }
